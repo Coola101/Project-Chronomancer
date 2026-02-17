c6a0e6778df8ab5b23960e05b15ecd724493fb7f
Blank-Dude2 <warner.mihalak@gmail.com>
2026-02-17 13:26:02 -0500

    Working on Stalking navigation

--- scripts/Enemy/EnemyControl.gd
+++ scripts/Enemy/EnemyControl.gd
@@ -8,44 +8,81 @@ enum EnemyState {
 
 @onready var navigation = $NavigationAgent3D
 @onready var currentState = EnemyState.Idling
+@onready var aggression: float = 0
 
 func _ready() -> void:
-	changeState(EnemyState.Idling)
+	changeState(EnemyState.Stalking)
 
-var playerLocation
+var playerLocation: Vector3
+var playerRID: RID
 var rng = RandomNumberGenerator.new()
 
 func update_target_location(location):
 	playerLocation = location
 
-var chaseSpeed = 7.5
-var stalkSpeed = 5
-var stalkRadius = 7.5
+func set_player_RID(pl):
+	playerRID = pl
+
+const chaseSpeed: float = 7.5
+const stalkSpeed: float = 2.5
+const stalkRadius: float = 15
+const difficulty: float = 1
+const threshold: float = 100
+
 
 func _physics_process(_delta):
-	var currentlocation = global_transform.origin
+	var currentLocation = global_transform.origin
 	match currentState:
 		EnemyState.Stalking:
-			if(navigation.target_reached):
+			if(navigation.target_reached || !navigation.is_target_reachable()):
 				navigation.target_position = getNewStalkTarget()
 			var nextLocation = navigation.get_next_path_position()
-			var newVelocity = (nextLocation-currentlocation).normalized() * stalkSpeed
+			var newVelocity = (nextLocation-currentLocation).normalized() * stalkSpeed
 			velocity = velocity.move_toward(newVelocity, 0.25)
 			move_and_slide()
-			#do things
+			
+			#Look for player
+			var space_state = get_world_3d().direct_space_state
+			var queery = PhysicsRayQueryParameters3D.create(currentLocation, playerLocation)
+			queery.exclude = [self, playerRID]
+			var result = space_state.intersect_ray(queery)
+			if(result.is_empty()):
+				print("Success")
+				changeState(EnemyState.Chasing)
+			else:
+				print(result.keys())
+				#aggression -= 1/difficulty
+				#if(aggression <= 0):
+					#changeState(EnemyState.Cooldown)
 		EnemyState.Chasing:
 			navigation.target_position = playerLocation
 			var nextLocation = navigation.get_next_path_position()
-			var newVelocity = (nextLocation-currentlocation).normalized() * chaseSpeed
+			var newVelocity = (nextLocation-currentLocation).normalized() * chaseSpeed
 			velocity = velocity.move_toward(newVelocity, 0.25)
 			move_and_slide()
 			#Current implemented AI
+		EnemyState.Stunned:
+			velocity = Vector3(0,0,0)
+			#recover time
 
 func getNewStalkTarget() -> Vector3:
-	var x = randf_range(playerLocation.x - stalkRadius, playerLocation.x + stalkRadius)
-	var z = randf_range(playerLocation.z - stalkRadius, playerLocation.z + stalkRadius)
-	var target = Vector3(x, playerLocation.y, z)
+	var x_pos = randf_range(playerLocation.x - stalkRadius, playerLocation.x + stalkRadius)
+	var z_pos = randf_range(playerLocation.z - stalkRadius, playerLocation.z + stalkRadius)
+	var target = Vector3(x_pos, playerLocation.y, z_pos)
 	return target
 	
 func changeState(newState: EnemyState):
+	match newState:
+		EnemyState.Stalking:
+			#emerge
+			getNewStalkTarget()
+		EnemyState.Chasing:
+			velocity = Vector3(0,0,0)
+			#play sound
+			#delay
 	currentState = newState
+
+func _sound_call(sound: float):
+	aggression += (sound * difficulty)
+	if(aggression >= threshold && currentState == EnemyState.Idling):
+		changeState(EnemyState.Stalking)
