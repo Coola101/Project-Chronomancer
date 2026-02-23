05a615519a19f901de9bbb0a7326a759f29bf44a
Alexander Gignac <agign001@odu.edu>
2026-02-23 12:52:08 -0500

    Added controls toggle to main menu

--- scripts/menu/MainMenu.gd
+++ scripts/menu/MainMenu.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+var showControls: bool = false
+@onready var controls_note = get_node("ControlsNote")
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
@@ -7,6 +10,24 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("full_screen"):
 		var is_window: bool = DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if is_window else DisplayServer.WINDOW_MODE_WINDOWED)
+	if Input.is_action_just_pressed("interact"):
+		showControls = !showControls
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var ref: Color = controls_note.get_modulate()
+	if showControls:
+		if ref.a < 255:
+			ref.a = move_toward(ref.a,255,delta*2)
+			controls_note.set_modulate(ref)
+		if controls_note.position.y > 15*(get_viewport().get_visible_rect().size.y/16):
+			controls_note.position.y = move_toward(controls_note.position.y,controls_note.get_viewport_rect().size.y/5,delta*550)
+	else:
+		if ref.a > 0:
+			ref.a = move_toward(ref.a,0,delta*4)
+			controls_note.set_modulate(ref)
+		if controls_note.position.y < (get_viewport().get_visible_rect().size.y*1.5):
+			controls_note.position.y = move_toward(controls_note.position.y,controls_note.get_viewport_rect().size.y*2,delta*550)
 
 func _on_button_start_pressed() -> void:
 	
