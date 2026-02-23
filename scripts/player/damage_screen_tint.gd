7e51a05c9906a7192b5c443202797c6a7ba03898
Alexander Gignac <agign001@odu.edu>
2026-02-23 13:03:58 -0500

    Added Fullscreen functions to damage_screen_tint.gd

--- scripts/player/damage_screen_tint.gd
+++ scripts/player/damage_screen_tint.gd
@@ -2,6 +2,10 @@ extends ColorRect
 
 @onready var enemy = get_tree().get_root().get_child(0).get_node("EnemyBody")
 
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("full_screen"):
+		var is_window: bool = DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if is_window else DisplayServer.WINDOW_MODE_WINDOWED)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
